86653ecbc7865e0a3c53384e6c87328712984cea
GlassK <glassk0724@gmail.com>
2020-10-09 23:53:36 +0900

    201009

---
+++ IO/1924.py
@@ -0,0 +1,11 @@
+#1924 2007년
+import sys
+
+x, y = map(int, sys.stdin.readlines().split())
+
+if 1 <= x <= 12 and 1 <= y <= 31:
+    
+
+
+else:
+    print("Check input condition: 1 <= x <= 12 and 1 <= y <= 31")
