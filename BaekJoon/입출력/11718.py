1bbe0ca61d5d9981f958ad05c16e129181fc15c1
GlassK <glassk0724@gmail.com>
2021-02-11 10:59:11 +0900

    파일 형식 변경

--- BaekJoon/입출력/11718.py
+++
@@ -1,8 +0,0 @@
-#11718 그대로 출력하기
-#check1: .strip()을 하지 않으면 \n까지 포함해서 함께 출력한다
-#check2: 여러 줄을 입력받으므로(입력하는 대로 출력하므로) sys.stdin.readline() 대신 sys.stdin
-import sys
-
-for line in sys.stdin:  
-    print(line.strip())
-#further way - 11719번 참고
--- BaekJoon/입출력/2438.py
+++
@@ -1,8 +0,0 @@
-#2438 별 찍기 - 1
-N = int(input())
-
-if 1 <= N <= 100:
-    for i in range(1, N+1):
-        print('*'*i)
-else:
-    print('Check input condition: 1 <= input <= 100')
