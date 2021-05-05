311c173e85663dd892789ff8f6cc22d8bccb1638
GlassK <glassk0724@gmail.com>
2021-05-05 12:48:07 +0900

    [5-9] 수열 추측하기

--- Inflearn/5_완전탐색(DFS)/수열 추측하기(순열, 파스칼 응용).py
+++ Inflearn/5_완전탐색(DFS)/수열 추측하기(순열, 파스칼 응용).py
@@ -1,5 +1,4 @@
 import sys
-sys.stdin = open("input.txt", "rt")
 
 
 def DFS(L, sum):
@@ -18,8 +17,8 @@ def DFS(L, sum):
 
 if __name__ == "__main__":
     n, f = map(int, input().split())
-    p = [0]*n
     b = [1]*n
+    p = [0]*n
     ch = [0]*(n+1)
     for i in range(1, n):
         b[i] = b[i-1]*(n-i)//i
