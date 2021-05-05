f187f12639d7087fe0e5364388c6bd811e092c89
GlassK <glassk0724@gmail.com>
2021-05-05 12:49:05 +0900

    [5-11] 수들의 조합

---
+++ Inflearn/5_완전탐색(DFS)/수들의 조합(DFS).py
@@ -0,0 +1,41 @@
+def DFS(L, s):
+    global count
+    if L == k:
+        sum = 0
+        for i in range(k):
+            sum += a[res[i]]
+        if sum % m == 0:
+            count += 1
+    else:
+        for i in range(s, n):
+            res[L] = i
+            DFS(L+1, i+1)
+
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    m = int(input())
+    res = [0]*k
+    count = 0
+    DFS(0, 0)
+    print(count)
+
+'''
+def DFS(L, s, sum):
+    global cnt
+    if L==k:
+        if sum%m==0:
+            cnt+=1
+    else:
+        for i in range(s, n):
+            DFS(L+1, i+1, sum+a[i])
+ 
+if __name__=="__main__":
+    n, k=map(int, input().split())
+    a=list(map(int, input().split()))
+    m=int(input())
+    cnt=0
+    DFS(0, 0, 0)
+    print(cnt)
+'''
