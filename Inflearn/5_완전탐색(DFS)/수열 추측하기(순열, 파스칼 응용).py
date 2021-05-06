ed86a505187bb2f650d39fe1f28f04816f78a372
GlassK <glassk0724@gmail.com>
2021-05-06 09:50:26 +0900

    [5-12] 수열 추측하기

--- Inflearn/5_완전탐색(DFS)/수열 추측하기(순열, 파스칼 응용).py
+++ Inflearn/5_완전탐색(DFS)/수열 추측하기(순열, 파스칼 응용).py
@@ -1,3 +1,4 @@
+import itertools as it
 import sys
 
 
@@ -23,3 +24,21 @@ if __name__ == "__main__":
     for i in range(1, n):
         b[i] = b[i-1]*(n-i)//i
     DFS(0, 0)
+
+
+# 라이브러리를 이용한 순열
+n, f = map(int, input().split())
+b = [1]*n
+cnt = 0
+for i in range(1, n):
+    b[i] = b[i-1]*(n-i)/i
+a = list(range(1, n+1))
+
+for tmp in it.permutations(a):
+    sum = 0
+    for L, x in enumerate(tmp):
+        sum += (x*b[L])
+    if sum == f:
+        for x in tmp:
+            print(x, end=' ')
+        break
