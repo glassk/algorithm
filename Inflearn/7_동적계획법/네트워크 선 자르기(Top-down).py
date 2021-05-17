1ada091d061e8f64039fdb391d757ecf69b07ac5
GlassK <glassk0724@gmail.com>
2021-05-17 09:57:41 +0900

    [7-2] 네트워크 선 자르기(Top-down)

---
+++ Inflearn/7_동적계획법/네트워크 선 자르기(Top-down).py
@@ -0,0 +1,14 @@
+def DFS(len):
+    if dy[len] > 0:
+        return dy[len]
+    if len == 1 or len == 2:
+        return len
+    else:
+        dy[len] = DFS(len-1) + DFS(len-2)
+        return dy[len]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    dy = [0]*(n+1)
+    print(DFS(n))
