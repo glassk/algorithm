6a51e4c4f13fc559a51972b1d1a5cac1e33d4070
GlassK <glassk0724@gmail.com>
2021-05-23 23:59:53 +0900

    [여름겨울] 소수 만들기

--- Programmars/Level_1/소수 만들기.py
+++ Programmars/Level_1/소수 만들기.py
@@ -8,7 +8,8 @@ def isPrime(n):
         return True
 
 
-def solution(nums):
+# 1번째 풀이
+def solution1(nums):
     answer = 0
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
@@ -17,6 +18,32 @@ def solution(nums):
                     answer += 1
     return answer
 
+
+# 2번째 풀이 (외부 라이브러리 없이 리스트에서 pick개 만큼 뽑는 조합 구하기)
+# 1번째 풀이보다 속도는 훨씬 느리지만... 조합 DFS 알고리즘 복습 차원에서 구현
+def checkSumOfPick(level, start, nums):
+    global answer
+    if level == pick:
+        if isPrime(sum(result)):
+            answer += 1
+    else:
+        for i in range(start, len(nums)):
+            result[level] = nums[i]
+            checkSumOfPick(level+1, i+1, nums)
+
+
+def solution2(nums):
+    global pick, answer, result
+    answer = 0
+    pick = 3
+    result = [0]*(len(nums)+1)
+    checkSumOfPick(0, 0, nums)
+
+    return answer
+
+
+# print(solution([1, 2, 3, 4]))  # 1
+# print(solution([1, 2, 7, 6, 4]))  # 4
 
 # ref
 def solution(nums):
