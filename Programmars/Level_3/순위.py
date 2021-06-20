12e1b2955b95b0f6e22ac2e8a24d1b2ca9796804
GlassK <glassk0724@gmail.com>
2021-06-20 10:59:05 +0900

    [그래프] 순위

---
+++ Programmars/Level_3/순위.py
@@ -0,0 +1,24 @@
+# ref: defaultdict, set(add, update) 활용
+from collections import defaultdict
+
+
+def solution(n, results):
+    answer = 0
+    win, lose = defaultdict(set), defaultdict(set)
+    for result in results:
+        lose[result[1]].add(result[0])
+        win[result[0]].add(result[1])
+
+    for i in range(1, n+1):
+        for winner in lose[i]:
+            win[winner].update(win[i])
+        for loser in win[i]:
+            lose[loser].update(lose[i])
+
+    for i in range(1, n+1):
+        if len(win[i]) + len(lose[i]) == n - 1:
+            answer += 1
+    return answer
+
+
+print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))  # 2
