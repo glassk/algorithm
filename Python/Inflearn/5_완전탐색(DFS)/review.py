1884d58fed2bc664b3b5ee42621cb958b1b2c4ac
GlassK <glassk0724@gmail.com>
2021-11-07 22:31:23 +0900

    [inf-py] 완전탐색

--- Python/Inflearn/5_완전탐색(DFS)/review.py
+++ Python/Inflearn/5_완전탐색(DFS)/review.py
@@ -214,3 +214,28 @@ if __name__ == "__main__":
     check = [0]*(n+1)
     dfs(0)
     print(count)
+
+
+# 수열 추측하기
+def dfs(L, sum):
+    if L == n and sum == f:
+        for x in p:
+            print(x, end=' ')
+        sys.exit(0)
+    else:
+        for i in range(1, n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[L] = i
+                dfs(L+1, sum+(p[L]*b[L]))
+                ch[i] = 0
+
+
+if __name__ == "__main__":
+    n, f = map(int, input().split())
+    b = [1]*n
+    p = [0]*n
+    ch = [0]*(n+1)
+    for i in range(1, n):
+        b[i] = b[i-1]*(n-i)//i
+    dfs(0, 0)
