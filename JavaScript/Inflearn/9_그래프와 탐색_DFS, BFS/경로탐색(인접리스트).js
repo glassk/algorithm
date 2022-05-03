a5247e8c3d1b05fc79f7a007226a229adb080809
glassk <glassk0724@gmail.com>
2022-05-03 09:52:18 +0900

    [인프런-js] DFS와 인접리스트로 그래프 경로 탐색

--- JavaScript/Inflearn/9_그래프와 탐색_DFS, BFS/경로탐색(인접리스트).js
+++ JavaScript/Inflearn/9_그래프와 탐색_DFS, BFS/경로탐색(인접리스트).js
@@ -1,45 +1,45 @@
-// function solution(n, arr) {
-//   let answer = 0;
-//   const graph = Array.from(Array(n + 1), () => Array());
-//   const check = Array.from({ length: n + 1 }, () => 0);
+function solution(n, arr) {
+  let answer = 0;
+  const graph = Array.from(Array(n + 1), () => Array());
+  const check = Array.from({ length: n + 1 }, () => 0);
 
-//   for (let [a, b] of arr) {
-//     graph[a].push(b);
-//   }
+  for (let [a, b] of arr) {
+    graph[a].push(b);
+  }
 
-//   function DFS(v) {
-//     if (v === n) answer++;
-//     else {
-//       const len = graph[v].length;
-//       for (let i = 0; i < len; i++) {
-//         if (check[graph[v][i]] === 0) {
-//           check[graph[v][i]] = 1;
-//           DFS(graph[v][i]);
-//           check[graph[v][i]] = 0;
-//         }
-//       }
-//     }
-//   }
+  function DFS(v) {
+    if (v === n) answer++;
+    else {
+      const len = graph[v].length;
+      for (let i = 0; i < len; i++) {
+        if (check[graph[v][i]] === 0) {
+          check[graph[v][i]] = 1;
+          DFS(graph[v][i]);
+          check[graph[v][i]] = 0;
+        }
+      }
+    }
+  }
 
-//   check[1] = 1;
-//   DFS(1);
+  check[1] = 1;
+  DFS(1);
 
-//   return answer;
-// }
+  return answer;
+}
 
-// console.log(
-//   solution(5, [
-//     [1, 2],
-//     [1, 3],
-//     [1, 4],
-//     [2, 1],
-//     [2, 3],
-//     [2, 5],
-//     [3, 4],
-//     [4, 2],
-//     [4, 5],
-//   ])
-// ); // 6
+console.log(
+  solution(5, [
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [2, 1],
+    [2, 3],
+    [2, 5],
+    [3, 4],
+    [4, 2],
+    [4, 5],
+  ])
+); // 6
 
 // 경로까지 구하기(path)
 function solution(n, arr) {
