8cb2b6ef8d75b3b118892ef9df9dd59e91852999
glassk <glassk0724@gmail.com>
2022-05-30 12:39:39 +0900

    다른 풀이 추가

--- JavaScript/Programmars/Level_1/나누어 떨어지는 숫자 배열.js
+++ JavaScript/Programmars/Level_1/나누어 떨어지는 숫자 배열.js
@@ -13,3 +13,19 @@ function solution(arr, divisor) {
 console.log(solution([5, 9, 7, 10], 5)); // [5, 10]
 console.log(solution([2, 36, 1, 3], 1)); // [1, 2, 3, 36]
 console.log(solution([3, 2, 6], 10)); // -1
+
+// 다른 풀이: push()
+function solution(arr, divisor) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+  answer.sort((a, b) => a - b);
+
+  if (answer.length === 0) {
+    answer.push(-1);
+  }
+  return answer;
+}
