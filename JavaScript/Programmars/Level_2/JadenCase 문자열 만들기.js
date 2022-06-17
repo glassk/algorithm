02b14f7598209f938658825a6d86bb6358c4626b
glassk <glassk0724@gmail.com>
2022-06-17 10:55:53 +0900

    [프로그래머스-js] 문자열 string.charAt()과 string[i] 차이

---
+++ JavaScript/Programmars/Level_2/JadenCase 문자열 만들기.js
@@ -0,0 +1,32 @@
+// 문제: https://programmers.co.kr/learn/courses/30/lessons/12951
+function solution(s) {
+  let answer = '';
+
+  let tempWord = '';
+  for (let char of s) {
+    if (char === ' ') {
+      if (tempWord.length > 0) {
+        answer += tempWord[0].toUpperCase() + tempWord.substr(1).toLowerCase();
+        tempWord = '';
+      }
+      answer += ' ';
+    } else tempWord += char;
+  }
+
+  if (tempWord.length > 0)
+    answer += tempWord[0].toUpperCase() + tempWord.substr(1).toLowerCase();
+
+  return answer;
+}
+
+console.log(solution('3people unFollowed me')); // "3people Unfollowed Me"
+console.log(solution('for the last week')); // "For The Last Week"
+
+// 다른 풀이: ✅ charAt() => 대상 문자열이 공백인 경우 빈 문자열 리턴 ([index]는 undefined 리턴)
+// 참고: https://thisthat.dev/string-char-at-vs-string-bracket-notation/
+function solution(s) {
+  return s
+    .split(' ')
+    .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
+    .join(' ');
+}
