3ac651ea3c16185c8fc29e8227e9e61ecb45e5d5
Yuri Kim <glassk0724@gmail.com>
2022-09-21 20:05:59 +0900

    [프로그래머스-js] 객체, Set

--- JavaScript/Programmars/카카오블라인드/신고 결과 받기.js
+++ JavaScript/Programmars/카카오블라인드/신고 결과 받기.js
@@ -24,13 +24,38 @@ function solution(id_list, report, k) {
   return answer;
 }
 
+// 다른 풀이: reportCount로 신고당한 횟수 따로 카운트
+function solution(id_list, report, k) {
+  const reportInfo = {};
+  const reportCount = {};
+
+  id_list.forEach((id) => {
+    reportInfo[id] = new Set(); // 신고된 유저 아이디 = { 신고한 유저 목록 }
+    reportCount[id] = 0; // 신고당한 횟수
+  });
+  report.forEach((v) => {
+    const [reporter, reported] = v.split(' ');
+    reportInfo[reported].add(reporter);
+  });
+
+  Object.values(reportInfo).forEach((reporters) => {
+    if (reporters.size >= k) {
+      for (let reporter of reporters) {
+        reportCount[reporter]++;
+      }
+    }
+  });
+
+  return Object.values(reportCount);
+}
+
 console.log(
   solution(
     ['muzi', 'frodo', 'apeach', 'neo'],
     ['muzi frodo', 'apeach frodo', 'frodo neo', 'muzi neo', 'apeach muzi'],
     2
   )
-); // [2,1,1,0]
+); // [ 2, 1, 1, 0 ]
 console.log(
   solution(['con', 'ryan'], ['ryan con', 'ryan con', 'ryan con', 'ryan con'], 3)
-); // [0, 0]
+); // [ 0, 0 ]
